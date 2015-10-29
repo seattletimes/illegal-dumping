1dd8092f18f7b713875692d81957215ddbf3c717
Audrey Carlsen <acarlsen@seattletimes.com>
2015-10-29 15:00:45 -0700

    esri map

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,3 @@
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
-require("component-leaflet-map");
-
-var L = require("leaflet");
-
-var mapElement = document.querySelector("leaflet-map");
-var L = mapElement.leaflet;
-var map = mapElement.map;
-
-dumpData.forEach(function(dump) {
-  var icon = new L.divIcon({className: 'pin'});
-  L.marker([dump.lat,dump.lng], {icon: icon}).addTo(map);
-});
